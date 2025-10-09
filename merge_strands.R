fddad1616130d9cd1d761ae0a99ff074a87a2b89
LGGray <lachlan.gray@tum.de>
2025-10-09 13:58:08 +0200

    refactor: streamline merge_strands.R and update SLURM scripts for improved file handling

--- merge_strands.R
+++ merge_strands.R
@@ -1,22 +1,19 @@
 ##*******************************##
 ##  Allelome.PRO  merge strands  ##
 ##*******************************##
-## Project: Allelome.Link
-## Tim Hasenbein
-## Last modification 01.2023
-## Creation: 12.2022
-## Merge fwd and reverse strand
-library(tidyverse)
+
 library(data.table)
 
-forward <- commandArgs(trailingOnly = TRUE)[1]
-reverse <- commandArgs(trailingOnly = TRUE)[2]
-output <- commandArgs(trailingOnly = TRUE)[3]
+sample <- commandArgs(trailingOnly = TRUE)[1]
+
+forward <- list.files(paste0('adult/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[1]
+reverse <- list.files(paste0('adult/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[2]
+
 
 # Merge locus table
 fwd_locus <- fread(forward, header = T)
 rev_locus <- fread(reverse, header = T)
-merge_locus <- fwd_locus%>%rbind(rev_locus)  
-merge_locus <- merge_locus%>%arrange(chr,start)
+merge_locus <- rbind(fwd_locus, rev_locus)
+merge_locus <- merge_locus[order(merge_locus$chr, merge_locus$start), ]
 
-write.table(merge_locus, paste0(output, '/merged_locus_table.txt'), col.names=T, row.names=F, sep="\t", quote=F)
+write.table(merge_locus, paste0('adult/', sample, '/merged_locus_table.txt'), col.names=T, row.names=F, sep="\t", quote=F)
