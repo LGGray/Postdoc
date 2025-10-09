4b336b26ad4cfbadfe02e2bef30b89278c6f0529
LGGray <lachlan.gray@tum.de>
2025-10-09 13:59:29 +0200

    fix: update file path handling in merge_strands.R to include age parameter

--- merge_strands.R
+++ merge_strands.R
@@ -4,10 +4,11 @@
 
 library(data.table)
 
-sample <- commandArgs(trailingOnly = TRUE)[1]
+age <- commandArgs(trailingOnly = TRUE)[1]
+sample <- commandArgs(trailingOnly = TRUE)[2]
 
-forward <- list.files(paste0('adult/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[1]
-reverse <- list.files(paste0('adult/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[2]
+forward <- list.files(paste0(age, '/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[1]
+reverse <- list.files(paste0(age, '/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[2]
 
 
 # Merge locus table
@@ -16,4 +17,4 @@ rev_locus <- fread(reverse, header = T)
 merge_locus <- rbind(fwd_locus, rev_locus)
 merge_locus <- merge_locus[order(merge_locus$chr, merge_locus$start), ]
 
-write.table(merge_locus, paste0('adult/', sample, '/merged_locus_table.txt'), col.names=T, row.names=F, sep="\t", quote=F)
+write.table(merge_locus, paste0(age, '/', sample, '/merged_locus_table.txt'), col.names=T, row.names=F, sep="\t", quote=F)
