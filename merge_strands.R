f116a57c2c4460192202e966be59fd1539adeb64
LGGray <lachlan.gray@tum.de>
2025-10-10 14:52:33 +0200

    fix: update SLURM script to include strand separation and processing steps for BAM files

--- merge_strands.R
+++ merge_strands.R
@@ -4,11 +4,26 @@
 
 library(data.table)
 
-age <- commandArgs(trailingOnly = TRUE)[1]
-sample <- commandArgs(trailingOnly = TRUE)[2]
+# age <- commandArgs(trailingOnly = TRUE)[1]
+# sample <- commandArgs(trailingOnly = TRUE)[2]
 
-forward <- list.files(paste0(age, '/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[1]
-reverse <- list.files(paste0(age, '/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[2]
+# forward <- list.files(paste0(age, '/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[1]
+# reverse <- list.files(paste0(age, '/', sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[2]
+
+
+# # Merge locus table
+# fwd_locus <- fread(forward, header = T)
+# rev_locus <- fread(reverse, header = T)
+# merge_locus <- rbind(fwd_locus, rev_locus)
+# merge_locus <- merge_locus[order(merge_locus$chr, merge_locus$start), ]
+
+# write.table(merge_locus, paste0(age, '/', sample, '/merged_locus_table.txt'), col.names=T, row.names=F, sep="\t", quote=F)
+
+
+sample <- commandArgs(trailingOnly = TRUE)[1]
+
+forward <- list.files(paste0(sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[1]
+reverse <- list.files(paste0(sample), pattern='locus_table.txt', recursive=T, full.names = TRUE)[2]
 
 
 # Merge locus table
@@ -17,4 +32,4 @@ rev_locus <- fread(reverse, header = T)
 merge_locus <- rbind(fwd_locus, rev_locus)
 merge_locus <- merge_locus[order(merge_locus$chr, merge_locus$start), ]
 
-write.table(merge_locus, paste0(age, '/', sample, '/merged_locus_table.txt'), col.names=T, row.names=F, sep="\t", quote=F)
+write.table(merge_locus, paste0(sample, '/merged_locus_table.txt'), col.names=T, row.names=F, sep="\t", quote=F)
